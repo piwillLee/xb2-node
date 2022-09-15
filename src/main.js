7288afa2a0f39f94af25c720e8d0a94f27f631ca
willlee <1301436989@qq.com>
2022-09-15 21:08:47 +0800

    express

--- src/main.js
+++ src/main.js
@@ -1,21 +1,45 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const server = http.createServer((request, response) => {
-  const data = {
+app.listen(port, () => {
+  console.log("🚀 服务已启动！");
+});
+
+app.get("/", (request, response) => {
+  response.send("你好！");
+});
+
+//演示数据
+const data = [
+  {
     id: 1,
     title: "关山月",
     content: "明月出天山，苍茫云海间",
-  };
+  },
+  {
+    id: 2,
+    title: "望岳",
+    content: "会当凌绝顶，一览众山小",
+  },
+  {
+    id: 3,
+    title: "忆江南",
+    content: "日出江花红似火，春来江水绿如蓝",
+  },
+];
 
-  const jsonData = JSON.stringify(data);
-
-  response.writeHead(200, {
-    "Content-Type": "application/json; charset=utf-8",
-  });
-
-  response.write(jsonData);
-  response.end();
+app.get("/posts", (request, response) => {
+  response.send(data);
 });
-server.listen(3000, () => {
-  console.log("服务已启动");
+
+app.get("/posts/:postId", (request, response) => {
+  //获取内容 ID
+  const { postId } = request.params;
+
+  //查找具体内容
+  const posts = data.filter((item) => item.id == postId);
+
+  //做出响应
+  response.send(posts[0]);
 });
