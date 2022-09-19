a73042e8c2e53dbab97e462e1797c27d27f8ae38
willlee <1301436989@qq.com>
2022-09-19 17:41:52 +0800

    设设置处理器参数的类型

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
@@ -12,7 +13,7 @@ app.listen(port, () => {
   console.log("🚀 服务已启动！");
 });
 
-app.get("/", (request, response) => {
+app.get("/", (request:Request, response:Response) => {
   response.send("你好！");
 });
 
@@ -35,11 +36,11 @@ const data = [
   },
 ];
 
-app.get("/posts", (request, response) => {
+app.get("/posts", (request:Request, response:Response) => {
   response.send(data);
 });
 
-app.get("/posts/:postId", (request, response) => {
+app.get("/posts/:postId", (request:Request, response:Response) => {
   //获取内容 ID
   const { postId } = request.params;
 
@@ -53,7 +54,7 @@ app.get("/posts/:postId", (request, response) => {
 /**
  *创建内容
  */
-app.post("/posts", (request, response) => {
+app.post("/posts", (request:Request, response:Response) => {
   //获取请求里的数据
   const { content } = request.body;
 
