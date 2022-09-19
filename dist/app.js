111f2dc606de98564299a3a229e0eaa56ed68c23
willlee <1301436989@qq.com>
2022-09-19 19:45:00 +0800

    javascript模块

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const greeting_1 = require("./greeting");
+(0, greeting_1.greet)();
+//# sourceMappingURL=app.js.map
---
+++ dist/greeting.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = void 0;
+const greet = () => {
+    console.log("您好 ~");
+};
+exports.greet = greet;
+//# sourceMappingURL=greeting.js.map
