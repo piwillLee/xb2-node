418bca69e60b2d57e0a6f4d16e07c59fb03bf363
willlee <1301436989@qq.com>
2022-09-30 11:50:43 +0800

    测试使用异常处理器

--- dist/post/post.controller.js
+++ dist/post/post.controller.js
@@ -3,6 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.index = void 0;
 const post_service_1 = require("./post.service");
 const index = (request, response, next) => {
+    if (request.headers.authorization !== "SECRET") {
+        return next(new Error());
+    }
     const posts = (0, post_service_1.getPosts)();
     response.send(posts);
 };
