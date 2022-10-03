6e3292d28d81703425df7b286bba111ed0b20f72
willlee <1301436989@qq.com>
2022-10-03 13:41:31 +0800

    处理使用数据服务时遇到的异常情况

--- dist/post/post.controller.js
+++ dist/post/post.controller.js
@@ -3,8 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.index = void 0;
 const post_service_1 = require("./post.service");
 const index = async (request, response, next) => {
-    const posts = await (0, post_service_1.getPosts)();
-    response.send(posts);
+    try {
+        const posts = await (0, post_service_1.getPosts)();
+        response.send(posts);
+    }
+    catch (error) {
+        next(error);
+    }
 };
 exports.index = index;
 //# sourceMappingURL=post.controller.js.map
--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -21,6 +21,10 @@ export const defaultErrorHandler = (
   response: Response,
   next: NextFunction
 ) => {
+  if (error.message) {
+    console.log("异常", error.message);
+  }
+
   let statusCode: number, message: string;
 
   /**
