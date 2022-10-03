6ba6c0365568708556e9dca9e6f41f58fc75ef33
willlee <1301436989@qq.com>
2022-10-03 10:12:46 +0800

    测试连接数据仓库

--- dist/main.js
+++ dist/main.js
@@ -5,7 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const app_config_1 = require("./app/app.config");
+const mysql_1 = require("./app/database/mysql");
 app_1.default.listen(app_config_1.APP_PORT, () => {
     console.log("🚀 服务已启动");
+});
+mysql_1.connection.connect((error) => {
+    if (error) {
+        console.log("连接数据服务失败", error.message);
+        return;
+    }
+    console.log("成功连接数据服务");
 });
 //# sourceMappingURL=main.js.map
