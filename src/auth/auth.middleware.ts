3d678e60edeb324e7d02193be273d2fa11c01ecc
willlee <1301436989@qq.com>
2022-10-05 16:06:31 +0800

    验证用户密码是否匹配

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import * as userService from "../user/user.service";
+import bcrypt from "bcrypt";
 
 /**
  * 验证用户登录数据
  */
- export const validateLoginData = async (
+export const validateLoginData = async (
   request: Request,
   response: Response,
   next: NextFunction
@@ -19,8 +20,12 @@ import * as userService from "../user/user.service";
   if (!password) return next(new Error("PASSWORD_IS_REQUIRED"));
 
   //验证用户名
-  const user = await userService.getUserByName(name);
+  const user = await userService.getUserByName(name, { password: true });
   if (!user) return next(new Error("USER_DOES_NOT_EXIST"));
+
+  //验证用户密码
+  const matched = await bcrypt.compare(password, user.password);
+if (!matched) return next(new Error("PASSWORD_DOES_NOT_MATCH"));
 
   //下一步
   next();
