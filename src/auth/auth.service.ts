3f3d2ce0ef9e9df122c715bcf0bff8511b9da2e8
willlee <1301436989@qq.com>
2022-10-06 10:55:41 +0800

    定义用户是否拥有指定资源的功能

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken";
+import { connection } from "src/app/database/mysql";
 import { PRIVATE_KEY } from "../app/app.config";
 
 /**
- * 签发信息
+ * 签发令牌
  */
 interface SignTokenOptions {
   payload?: any;
@@ -17,4 +18,33 @@ export const signToken = (options: SignTokenOptions) => {
 
   //提供 JWT
   return token;
+};
+
+/**
+ * 检查用户是否拥有指定资源
+ */
+interface ProcessOptions {
+  resourceId: number;
+  resourceType: string;
+  userId: number;
+}
+
+export const process = async (options: ProcessOptions) => {
+  // 准备资源
+  const { resourceId, resourceType, userId } = options;
+
+  // 准备查询
+  const statement = `
+    SELECT COUNT(${resourceType}.id) as count
+    FROM ${resourceType}
+    WHERE &{resourceType}.id = ? AND userId = ?
+  `;
+
+  // 检查拥有权
+  const [data] = await connection
+    .promise()
+    .query(statement, [resourceId, userId]);
+
+  // 提供检查结果
+  return data[0].count ? true : false;
 };
