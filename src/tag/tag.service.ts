ab1580427b320d17f47ed208d54c79dc4b15e5ac
willlee <1301436989@qq.com>
2022-10-09 09:32:46 +0800

    定义存储标签数的功能

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,1 +1,20 @@
 import { connection } from "../app/database/mysql";
+import { TagModel } from "./tag.model";
+
+/**
+ * 创建标签
+ */
+export const creatTag = async (
+  tag:TagModel
+) => {
+  // 准备查询
+  const statement = `
+    INSERT INTO tag
+    set ?
+  `
+  // 执行查询
+  const [data] = await connection.promise().query(statement, tag)
+  
+  // 提供数据
+  return data as any;
+};
