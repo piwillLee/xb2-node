b8b78a9e3a8be92f8bc8c36f167c654a50e3ccd8
willlee <1301436989@qq.com>
2022-10-09 09:39:29 +0800

    定义按标签名称查找标签的功能

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -4,17 +4,32 @@ import { TagModel } from "./tag.model";
 /**
  * 创建标签
  */
-export const creatTag = async (
-  tag:TagModel
-) => {
+export const creatTag = async (tag: TagModel) => {
   // 准备查询
   const statement = `
     INSERT INTO tag
     set ?
-  `
+  `;
   // 执行查询
-  const [data] = await connection.promise().query(statement, tag)
-  
+  const [data] = await connection.promise().query(statement, tag);
+
   // 提供数据
   return data as any;
+};
+
+/**
+ * 按名字查找标签
+ */
+export const getTagByName = async (tagName: string) => {
+  // 准备查询
+  const statement = `
+  SELECT id, name FROM tag
+  WHERE name= ?
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, tagName);
+
+  // 提供数据
+  return data[0];
 };
